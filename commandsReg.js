cbda554d9b7165fcd1098e241081fdcab1cb4907
Nicolas CORRIERI <shirosuit@gmail.com>
2021-03-31 16:19:22 +0200

    Test 5

--- commandsReg.js
+++ commandsReg.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const botToken = 'Nzg1MjE3MzY0NzY5NjM2MzYy.X80oqA.y146FnMh-WoR-pvcncFSnXt6Uf8';
 
 url = "https://discord.com/api/v8/applications/785217364769636362/guilds/304660884482162688/commands";
 
@@ -8,13 +9,23 @@ json = {
 }
 
 // For authorization, you can use either your bot token
-headers = {
-    "Authorization": "Bot Nzg1MjE3MzY0NzY5NjM2MzYy.X80oqA.y146FnMh-WoR-pvcncFSnXt6Uf8",
-    'Content-Type': 'application/json'
-}
+// headers = {
+//     "Authorization": "Bot Nzg1MjE3MzY0NzY5NjM2MzYy.X80oqA.y146FnMh-WoR-pvcncFSnXt6Uf8",
+//     'Content-Type': 'application/json'
+// }
 // // or a client credentials token for your app with the applications.commands.update scope
 // headers = {
 //     "Authorization": "Bearer abcdefg"
 // }
-r = await fetch(url,headers=headers,json=json);
-console.log(r);
+r = fetch(url,{
+    method: 'post',
+    body: JSON.stringify(json),
+    headers: {
+      'Authorization': 'Bot ' + botToken,
+      'Content-Type': 'application/json'
+    }
+});
+setTimeout(function(){
+    console.log(r);
+},10000);
+    
