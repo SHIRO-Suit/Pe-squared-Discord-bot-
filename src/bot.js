709d7d9fcc802f7b9d1abd14a4fcfba64dd65aa6
Nicolas Corrieri <shirosuit@gmail.com>
2021-05-31 23:44:18 +0200

    fix json fetch

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const { Client } = require('discord.js');
 const { commands, events } = require('../commandsChecker.js');
+const json = require('../commandsFetchList.json');
 const client = new Client();
 module.exports = {client};
 
 (async () => {
   await client.login(process.env.TOKEN);
-  await client.api.applications('785217364769636362').guilds('304660884482162688').commands.post(require('../commandsFetchList.json'));
+  await client.api.applications('785217364769636362').guilds('304660884482162688').commands.post(json);
   client.ws.on('INTERACTION_CREATE', async interaction => {
     commands(interaction);
   });
