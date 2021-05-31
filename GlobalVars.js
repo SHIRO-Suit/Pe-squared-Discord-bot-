73be01c493333c65ceacbc27eb9a797e85c88a48
Nicolas Corrieri <shirosuit@gmail.com>
2021-05-31 22:50:15 +0200

    remove slappey architecture / use .env

--- GlobalVars.js
+++ GlobalVars.js
@@ -1,6 +1,3 @@
 var IsChiant = false;
-//var GlobalTimer;
-//module.exports.GlobalTimer = GlobalTimer;
 module.exports.IsChiant = IsChiant;
-// remove partie ischiant if dont work orrible anglishe
 
--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,11 @@
 require('dotenv').config();
 //import('../commandsReg.js');
-const { Client,Message } = require('discord.js');
-const { registerCommands, registerEvents } = require('./utils/registry');
+const { Client } = require('discord.js');
 const { commands, events } = require('../commandsChecker.js');
 const client = new Client();
 module.exports = {client};
 
 (async () => {
-  client.commands = new Map();
-  client.events = new Map();
-  client.prefix = process.env.DISCORD_BOT_PREFIX;
-  await registerCommands(client, '../commands');
-  await registerEvents(client, '../events');
   await client.login(process.env.DISCORD_BOT_TOKEN);
   await client.api.applications('785217364769636362').guilds('304660884482162688').commands.post({data: {
     name: 'ping',
