4499ac3c04482dea2a5db01683e785bbcc38874a
SHIRO-Suit <43580252+SHIRO-Suit@users.noreply.github.com>
2021-07-28 12:08:53 +0200

    Update updatevideos.js

--- interactions/updatevideos.js
+++ interactions/updatevideos.js
@@ -16,7 +16,7 @@ async function updatevideos(interaction){
       }});
      // let adminRole = client.guilds.cache.get(interaction["guild_id"]).roles.cache.find(role => role.name === "YouTubers B&S");
     let mask = 1<<4;
-    if(!(interaction.member.permissions & mask) == 0){
+    if((interaction.member.permissions & mask) == 0){
         message = "🚫 Vous n'avez pas la permission d'effectuer cette commande";
         endMessage();
         return;
